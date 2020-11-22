ea9593569b1dbae6aef75d4a6ea8c5e686faf639
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-11-22 18:11:02 +0900

    First commit

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+var count = 100;
 export default function App() {
   return (
+
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{count}</Text>
       <StatusBar style="auto" />
     </View>
+
+
+
+
   );
+
 }
 
 const styles = StyleSheet.create({
