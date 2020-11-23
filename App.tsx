4231fcd7ccb5bcd3257344511fcb7d60477dc1fe
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-11-23 12:58:45 +0900

    画像表示のみ可

--- App.tsx
+++ App.tsx
@@ -1,22 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,Image,Pressable } from 'react-native';
+import logo1 from './assets/egg.images/egg01.png';
+import logo2 from './assets/egg.images/egg02.png';
+import logo3 from './assets/egg.images/egg03.png';
 
-var count = 100;
+let imgArry = [logo1,logo2,logo3];
+var imgNum = imgArry.length
+
 export default function App() {
+  var [count, setCount] = useState(10);
+  
+  
+  imgNum = 1;
+  
+  if (count == 5) {
+    imgNum += 1;
+  } else if (count == 0) {
+    imgNum += 1;
+  }; 
+  
   return (
-
-    
     <View style={styles.container}>
+      <Pressable
+        onPress={() => {
+          setCount((current) => current - 1);
+        }}
+        >
+        <Image
+          source={imgNum}
+          style={{
+            width: 200,
+            height: 200,
+            resizeMode: "contain",
+          }}
+          />
+
       <Text>{count}</Text>
       <StatusBar style="auto" />
+      </Pressable>
     </View>
-
-
-
-
   );
-
+  
 }
 
 const styles = StyleSheet.create({
@@ -27,3 +52,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// switch(imgNum) {
+//   case count = 5:
+//     imgNum = +1;
+//     break;
+//   case count = 0 :
+//     imgNum = + 1;
+//     break;
+//     default:
+// }
