eb6715133639e4f89ef88985b3e906a92f82c91c
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-11-26 11:24:45 +0900

    モンスターの配列追加

--- App.tsx
+++ App.tsx
@@ -4,44 +4,67 @@ import { StyleSheet, Text, View,Image,Pressable } from 'react-native';
 import logo1 from './assets/egg.images/egg01.png';
 import logo2 from './assets/egg.images/egg02.png';
 import logo3 from './assets/egg.images/egg03.png';
+import monster1 from './assets/attak1.png'
+import monster2 from './assets/spartcamp.png'
+import monster3 from './assets/ritch02.png'
+
 
 let imgArry = [logo1,logo2,logo3];
-var imgNum = imgArry.length
+let imgNum = imgArry.length
+let msg = ""
+let resultImasge = imgArry[imgNum] || rand
+
+
+
+
+let monster = [monster1,monster2,monster3];
+var rand = monster[ Math.floor( Math.random() * monster.length ) ] ;
+
+
 
 export default function App() {
-  var [count, setCount] = useState(10);
-  
-  
-  imgNum = 1;
-  
-  if (count == 5) {
-    imgNum += 1;
-  } else if (count == 0) {
-    imgNum += 1;
-  }; 
-  
+  var [count, setCount] = useState(100);
+  resultImasge = imgArry[0];
+
+  if (count <= 50 && count > 0) {
+    resultImasge = imgArry[1];
+  }
+  if (count <= 0) {
+    resultImasge = rand;
+  } 
+
+  if (count === 75) {
+    msg = "残り75";
+  }else if (count === 50) {
+    msg = "あと半分";
+  }else if (count === 25) {
+    msg = "あとすこし！";
+  }else if (count === 0) {
+    msg = "モンスターが生まれました🐣";
+  }
+
   return (
     <View style={styles.container}>
       <Pressable
         onPress={() => {
           setCount((current) => current - 1);
         }}
-        >
+      >
+        <Text style={{ color: "#195", fontSize: 18 }}>{count}</Text>
+        <StatusBar style="auto" />
+
         <Image
-          source={imgNum}
+          source={resultImasge}
           style={{
             width: 200,
             height: 200,
             resizeMode: "contain",
           }}
-          />
-
-      <Text>{count}</Text>
-      <StatusBar style="auto" />
+        />
+        <Text>{msg}</Text>
       </Pressable>
     </View>
   );
-  
 }
 
 const styles = StyleSheet.create({
@@ -54,12 +77,11 @@ const styles = StyleSheet.create({
 });
 
 
-// switch(imgNum) {
-//   case count = 5:
-//     imgNum = +1;
-//     break;
-//   case count = 0 :
-//     imgNum = + 1;
-//     break;
-//     default:
+
+
+
+// if (count <= 50 && count <= 0 ) {
+//   imgNum === 1;
+// } else if (count === 0){
+//   imgNum === 2;  
 // }
